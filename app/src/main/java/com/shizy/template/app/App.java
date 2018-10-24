6f91672e1f900e7372af9684a9fa50cd99893f01
熊大虎 <397601035@qq.com>
2018-10-24 15:27:32 +0800

    add bugly

---
+++ app/src/main/java/com/shizy/template/app/App.java
@@ -0,0 +1,20 @@
+package com.shizy.template.app;
+
+import com.tencent.tinker.loader.app.TinkerApplication;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+public class App extends TinkerApplication {
+
+	public App() {
+		//tinkerFlags 表示Tinker支持的类型 dex only、library only or all suuport，default: TINKER_ENABLE_ALL
+		//delegateClassName Application代理类 自定义的BaseApplicationLike
+		//loaderClassName Tinker的加载器，使用默认TinkerLoader即可
+		//tinkerLoadVerifyFlag 加载dex或者lib是否验证md5，默认为false
+
+		// TODO: 2018/10/24
+		super(ShareConstants.TINKER_ENABLE_ALL, "com.shizy.template.app.ApplicationLike",
+				"com.tencent.tinker.loader.TinkerLoader", false);
+	}
+
+
+}
