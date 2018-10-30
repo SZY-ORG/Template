0fbfd5f69b57bc5b604af9ded3b9a73be0c12f36
熊大虎 <397601035@qq.com>
2018-10-30 10:10:30 +0800

    StatisticsConstant

---
+++ app/src/main/java/com/shizy/template/common/mtj/StatisticsConstant.java
@@ -0,0 +1,15 @@
+package com.shizy.template.common.mtj;
+
+/**
+ * description
+ *
+ * @author dahu
+ * time 2018/10/29 17:50.
+ */
+public interface StatisticsConstant {
+
+	String TAB_TASK_HALL = "tab_task_hall";
+	String TAB_WORKBENCH = "tab_workbench";
+	String TAB_PERSONAL_CENTER = "tab_personal_center";
+
+}
