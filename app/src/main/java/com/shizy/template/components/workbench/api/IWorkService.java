18e97a6db8d5f63aa1a12a84c1018d38289103fa
熊大虎 <397601035@qq.com>
2018-10-30 18:04:45 +0800

    add api and bean packages

---
+++ app/src/main/java/com/shizy/template/components/workbench/api/IWorkService.java
@@ -0,0 +1,16 @@
+package com.shizy.template.components.workbench.api;
+
+import com.shizy.template.common.bean.Pagination;
+import com.shizy.template.components.taskhall.bean.Task;
+import com.shizy.template.net.response.ResponseData;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IWorkService {
+
+	@GET("api/task/list.json")
+	Observable<ResponseData<Pagination<Task>>> getTaskList(@Query("timestamp") String timestamp);
+
+}
---
+++ app/src/main/java/com/shizy/template/components/workbench/bean/Work.java
@@ -0,0 +1,15 @@
+package com.shizy.template.components.workbench.bean;
+
+import com.shizy.template.common.bean.BaseBean;
+
+/**
+ * description
+ *
+ * @author dahu
+ * time 2018/10/30 17:03.
+ */
+public class Work extends BaseBean {
+
+	private static final long serialVersionUID = 1057730897892390587L;
+
+}
