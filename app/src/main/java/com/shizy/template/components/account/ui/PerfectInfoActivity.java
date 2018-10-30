859780b2fa04341abfee837adf63e7b1474f8b76
史忠勇 <shizhongyong@yunniao.me>
2018-10-30 18:07:59 +0800

    增加完善信息页布局

---
+++ app/src/main/java/com/shizy/template/components/account/ui/PerfectInfoActivity.java
@@ -0,0 +1,16 @@
+package com.shizy.template.components.account.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.shizy.template.R;
+import com.shizy.template.common.view.activity.BaseTitleActivity;
+
+public class PerfectInfoActivity extends BaseTitleActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_perfect_info);
+	}
+}
