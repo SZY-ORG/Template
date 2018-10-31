2f4539362d1e7b7294b82343329284065dc8cb0c
熊大虎 <397601035@qq.com>
2018-10-31 18:23:28 +0800

    流水详情

--- app/src/main/java/com/shizy/template/components/personalcenter/finance/CapitalFlowDetailActivity.java
+++ app/src/main/java/com/shizy/template/components/personalcenter/finance/CapitalFlowDetailActivity.java
@@ -2,9 +2,14 @@ package com.shizy.template.components.personalcenter.finance;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.shizy.template.R;
+import com.shizy.template.common.utils.ClickUtil;
+import com.shizy.template.common.utils.ToastUtil;
 import com.shizy.template.common.view.activity.BaseTitleActivity;
+
+import butterknife.OnClick;
 
 /**
  * description
@@ -25,4 +30,18 @@ public class CapitalFlowDetailActivity extends BaseTitleActivity {
 		setTitle(R.string.business_list);
 
 	}
+
+	@OnClick({R.id.have_question})
+	protected void onClick(View view) {
+		if (!ClickUtil.isValid()) {
+			return;
+		}
+		switch (view.getId()) {
+			// 对交易有疑问
+			case R.id.have_question:
+				ToastUtil.showShort("对交易有疑问");
+				break;
+		}
+	}
+
 }
